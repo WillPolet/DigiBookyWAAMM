9cb9e927ff11241aa509ed04cc7804d26479568f
WillPolet <williamplomac@gmail.com>
2024-04-04 15:09:45 +0200

    Creation of the project with Spring Initializr

---
+++ DigiBooky/src/main/java/com/switchfully/digibooky/DigiBookyApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.digibooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiBookyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiBookyApplication.class, args);
+	}
+
+}
