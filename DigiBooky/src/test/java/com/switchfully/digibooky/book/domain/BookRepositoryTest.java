6e4c2cf461059c4edb9ee580dcabb021e74df3b6
Michael Koene <michael.koene@skynet.be>
2024-04-05 19:37:18 +0200

    Test : Search by title, isbn and titleAndIsbn implemented

---
+++ DigiBooky/src/test/java/com/switchfully/digibooky/book/domain/BookRepositoryTest.java
@@ -0,0 +1,85 @@
+package com.switchfully.digibooky.book.domain;
+
+import com.switchfully.digibooky.author.domain.Author;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+class BookRepositoryTest {
+
+    private static final Author AUTHOR1 = new Author();
+    private static final Book BOOK1 = new Book("is1bn1", "title1", "summary", true, false, AUTHOR1);
+    private static final Book BOOK2 = new Book("isbn2", "title2super", "summary", true, false, AUTHOR1);
+    private static final Book BOOK3 = new Book("isbn3", "3title", "summary", true, false, AUTHOR1);
+    private static final List<Book> BOOKS = List.of(BOOK1, BOOK2, BOOK3);
+
+    private final BookRepository bookRepository = new BookRepository();
+
+    @Test
+    void givenTitle_whenSearchBookWithTitleNotExisting_thenReturn0Book() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByTitleWithWildcard("bla")).contains();
+    }
+
+    @Test
+    void givenTitle_whenSearchBookWithExactTitle_thenReturnTheGoodBook() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByTitleWithWildcard("title1")).contains(BOOK1);
+    }
+
+    @Test
+    void givenTitle_whenSearchBookWithWildcardTitle_thenReturnMatchedBook() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByTitleWithWildcard("title*")).contains(BOOK1, BOOK2);
+    }
+
+    @Test
+    void givenISBN_whenSearchBookWithTitleNotExisting_thenReturn0Book() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByIsbnWithWildcard("bla")).contains();
+    }
+
+    @Test
+    void givenISBN_whenSearchBookWithExactTitle_thenReturnTheGoodBook() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByIsbnWithWildcard("is1bn")).contains(BOOK1);
+    }
+
+    @Test
+    void givenISBN_whenSearchBookWithWildcardTitle_thenReturnMatchedBook() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByIsbnWithWildcard("isbn*")).contains(BOOK2, BOOK3);
+    }
+
+    @Test
+    void givenTitleAndISBN_whenSearchBookNotExisting_thenReturn0Book() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByTitleAndIsbnWithWildcard("bla", "bla")).contains();
+    }
+
+    @Test
+    void givenTitleAndISBN_whenSearchBookWithExistingTitleButNotExistingIsbn_thenReturn0Book() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByTitleAndIsbnWithWildcard("title1", "bla")).contains();
+    }
+
+    @Test
+    void givenTitleAndISBN_whenSearchBookWithExistingIsbnButNotExistingTitle_thenReturn0Book() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByTitleAndIsbnWithWildcard("bla", "isbn2")).contains();
+    }
+
+    @Test
+    void givenTitleAndISBN_whenSearchBookTitleAndIsbn_thenReturnBooks() {
+        addBooksToRepository();
+        Assertions.assertThat(bookRepository.searchBooksByTitleAndIsbnWithWildcard("*title", "isbn*")).contains(BOOK3);
+    }
+
+    private void addBooksToRepository() {
+        bookRepository.addBook(BOOK1);
+        bookRepository.addBook(BOOK2);
+        bookRepository.addBook(BOOK3);
+    }
+}
---
+++ DigiBooky/src/test/java/com/switchfully/digibooky/book/service/BookServiceTest.java
@@ -0,0 +1,50 @@
+package com.switchfully.digibooky.book.service;
+
+import com.switchfully.digibooky.author.domain.Author;
+import com.switchfully.digibooky.book.domain.Book;
+import com.switchfully.digibooky.book.domain.BookRepository;
+import com.switchfully.digibooky.book.service.dto.BookDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class BookServiceTest {
+
+    private static final Author AUTHOR = new Author();
+    private static final Book BOOK = new Book("isbn", "title", "summary", true, false, AUTHOR);
+    private static final BookDto BOOK_DTO = new BookDto(BOOK.getUuid(), BOOK.getIsbn(), BOOK.getTitle(), BOOK.getSummary(), BOOK.getAccessible(), BOOK.getRented(), BOOK.getAuthor());
+    @Mock
+    BookMapper bookMapper;
+    @Mock
+    BookRepository bookRepository;
+    @InjectMocks
+    BookService bookService;
+
+    @Test
+    void givenTitle_whenSearchBookByTitle_thenReturnBook() {
+        Mockito.when(bookRepository.searchBooksByTitleWithWildcard(BOOK.getTitle())).thenReturn(List.of(BOOK));
+        Mockito.when(bookMapper.toDTO(List.of(BOOK))).thenReturn(List.of(BOOK_DTO));
+        Assertions.assertThat(bookService.searchBookByTitle(BOOK.getTitle())).isEqualTo(List.of(BOOK_DTO));
+    }
+
+    @Test
+    void givenIsbn_whenSearchBookByIsbn_thenReturnBook() {
+        Mockito.when(bookRepository.searchBooksByIsbnWithWildcard(BOOK.getIsbn())).thenReturn(List.of(BOOK));
+        Mockito.when(bookMapper.toDTO(List.of(BOOK))).thenReturn(List.of(BOOK_DTO));
+        Assertions.assertThat(bookService.searchBookByIsbn(BOOK.getIsbn())).isEqualTo(List.of(BOOK_DTO));
+    }
+
+    @Test
+    void givenTitleAndIsbn_whenSearchBookByTitleAndIsbn_thenReturnBook() {
+        Mockito.when(bookRepository.searchBooksByTitleAndIsbnWithWildcard(BOOK.getTitle(), BOOK.getIsbn())).thenReturn(List.of(BOOK));
+        Mockito.when(bookMapper.toDTO(List.of(BOOK))).thenReturn(List.of(BOOK_DTO));
+        Assertions.assertThat(bookService.searchBookByTitleAndIsbn(BOOK.getTitle(), BOOK.getIsbn())).isEqualTo(List.of(BOOK_DTO));
+    }
+}
